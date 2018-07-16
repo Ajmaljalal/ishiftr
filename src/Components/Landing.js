0fb55fd0c06e9420a7162cb59be0cfab9511e0bb
astro11 <aquila.strother@asu.edu>
2018-07-16 15:06:28 -0400

    creation of placeholder components

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -8,20 +8,21 @@ class Landing extends Component {
         return (
             <div>
                 <div className="signUp">
-                    <Button color="primary">Sign Up</Button>
-                    <Button color="primary">Sign In</Button>
+                    <Button classname="signUp" color="primary">Sign Up</Button>
+                    <Button classname="signUp" color="primary">Sign In</Button>
                 </div>
                 <div className="landing-img">
                     <img src = {require('../assets/imgLanding.jpg')}/>
                 </div>
                 <div className="landing-text">
                     <p>
-                        <h4>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.
-                        </h4>
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.                       
                     </p>
+                    <div>
+                        <Button className="scheduleButton" color="primary">Schedule Now</Button>
+                    </div>
                 </div>
 
             </div>
